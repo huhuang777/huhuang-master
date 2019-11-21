dfc164d422b1f1d14376b343a1749188466cd060
huhuang <652235206@qq.com>
2019-11-21 16:06:03 +0800

    增加底部备案

--- plugins/analytics.js
+++ plugins/analytics.js
@@ -28,39 +28,39 @@ if (isBrowser) {
   })
 }
 
-// if (isBrowser) {
-//   /*
-//   ** Google 统计分析脚本
-//   */
-//   ;(function(i, s, o, g, r, a, m) {
-//     i['GoogleAnalyticsObject'] = r
-//     ;(i[r] =
-//       i[r] ||
-//       function() {
-//         ;(i[r].q = i[r].q || []).push(arguments)
-//       }),
-//       (i[r].l = 1 * new Date())
-//     ;(a = s.createElement(o)), (m = s.getElementsByTagName(o)[0])
-//     a.async = 1
-//     a.src = g
-//     m.parentNode.insertBefore(a, m)
-//   })(window, document, 'script', '/scripts/analytics.js', 'ga')
-//   /*
-//   ** 当前页的访问统计
-//   */
-//   ga('create', 'UA-128592903-1', 'auto')
-//   // ga('send', 'pageview')
-// }
+if (isBrowser) {
+  /*
+  ** Google 统计分析脚本
+  */
+  ;(function(i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r
+    ;(i[r] =
+      i[r] ||
+      function() {
+        ;(i[r].q = i[r].q || []).push(arguments)
+      }),
+      (i[r].l = 1 * new Date())
+    ;(a = s.createElement(o)), (m = s.getElementsByTagName(o)[0])
+    a.async = 1
+    a.src = g
+    m.parentNode.insertBefore(a, m)
+  })(window, document, 'script', '/scripts/analytics.js', 'ga')
+  /*
+  ** 当前页的访问统计
+  */
+  ga('create', 'UA-128592903-1', 'auto')
+  // ga('send', 'pageview')
+}
 
-// export default ({ app: { router }, store }) => {
-//   /*
-//   ** 每次路由变更时进行pv统计
-//   */
-//   router.afterEach((to, from) => {
-//     /*
-//     ** 告诉 GA 增加一个 PV
-//     */
-//     ga('set', 'page', to.fullPath)
-//     ga('send', 'pageview')
-//   })
-// }
+export default ({ app: { router }, store }) => {
+  /*
+  ** 每次路由变更时进行pv统计
+  */
+  router.afterEach((to, from) => {
+    /*
+    ** 告诉 GA 增加一个 PV
+    */
+    ga('set', 'page', to.fullPath)
+    ga('send', 'pageview')
+  })
+}
